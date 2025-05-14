4a98cf859e69f9289eb84f46c8e1f71ea69fd380
Aaron Muhindo <muhindoaaron0@gmail.com>
2025-05-15 01:25:15 +0200

    resolutions des exercices par Aaron Muhindo

--- 01-variables/exercice_1/main.js
+++ 01-variables/exercice_1/main.js
@@ -1,1 +1,2 @@
+// Ce programme affiche un message Hello Word !
 console.log("Hello World !");
--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,6 +1,33 @@
 let paul, marie, eric, clair, madameMukuna, joseph, sarah;
 // Ecrivez votre code ici
 
+//Declaration de l'heritage 
+let maisonFamiliale = 60000000
+let terrainsEstimes = 40000000
+let liquidites = 20000000
+
+let heritageTotal = maisonFamiliale + terrainsEstimes + liquidites
+
+// repartition pour la premiere categorie 
+let premiereCate = heritageTotal * 75 / 100 
+// la premiere categorie de l'heritage doit etre partager en 3 parts egales 
+let laPart = premiereCate / 3 
+paul = laPart
+marie = laPart 
+// Pour les representants du defunt il faudra diviser la part a deux 
+eric = laPart / 2
+clair = laPart / 2
+
+// repartition pour la deuxieme categorie 
+let deuxiemeCate = heritageTotal * 25 / 100 
+// celle ci doit encore etre redistribue a 3 personnes 
+let laPart2 = deuxiemeCate / 3 
+madameMukuna = laPart2 
+joseph = laPart2
+sarah = laPart2
+
+
+
 // Ne pas modifier le code ci-dessous
 module.exports = {
   paul,
