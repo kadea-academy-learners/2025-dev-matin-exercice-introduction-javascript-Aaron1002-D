1902fc508ff1c9686b2d55036bb4399d44877853
Aaron Muhindo <muhindoaaron0@gmail.com>
2025-05-15 01:25:15 +0200

    resolutions des exercices par Aaron Muhindo

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -6,7 +6,24 @@ let salaireMensuel,
   totalDepenses,
   reste;
 // Ecrivez votre code ici
+salaireMensuel = 500
+loyer = salaireMensuel * 30 / 100
+nourriture = salaireMensuel * 20 / 100
+transport = salaireMensuel * 10 / 100
+autresDepenses = 50 
+autresDepenses += 25 
+totalDepenses = loyer + nourriture + transport + autresDepenses 
+reste = salaireMensuel - totalDepenses
 
+if ( reste >= 100 ) {
+  console.log ( " budget est bon")
+} else {
+  console.log(" attention, budjet serré")
+}
+
+let loisirs = salaireMensuel * 15 / 100
+totalDepenses += loisirs 
+reste = salaireMensuel - totalDepenses
 // Ne pas modifier le code ci-dessous
 module.exports = {
   salaireMensuel,
